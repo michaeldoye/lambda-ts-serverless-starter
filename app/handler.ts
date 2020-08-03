01be667d3674cbe5243e489025af600a679e2586
Michael Doye <mdoye@mobiquityinc.com>
2020-08-03 15:37:33 +0200

    chore(TSLambdaStarter): update serverless.yaml

--- app/handler.ts
+++ app/handler.ts
@@ -9,18 +9,14 @@ import { unknownError } from './utils/helpers';
 const myCoolController = container.resolve<MyCoolController>(MyCoolController);
 
 /**
- * ### GetAuthUrl
+ * ### getSomeStuffFromAPI
  *
- * Initiates the consent flow for the specified bank and
- * handles the initial connection to the bank, usually to request and authorisation code
- * which is used to begin the OAuth2.0 flow
+ * Describe your lambda here (you can use markdown to render code snippets in docs (npm run docs))
  *
- * Saves those accounts to DynamoDB
- *
- * @param event  AWS event object. MUST contain a string representing the Bank ID
+ * @param event  AWS event object.
  *
  */
-export const GetSomeStuffFromAPI: Handler = (event: MyCoolAWSEvent) => {
+export const getSomeStuffFromAPI: Handler = (event: MyCoolAWSEvent) => {
   if (event) {
     return myCoolController.handleSomeCoolControlFlow(event);
   }
--- tests/get-some-stuff-from-api.test.ts
+++ tests/get-some-stuff-from-api.test.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata';
 import { expect } from 'chai';
-import { GetSomeStuffFromAPI } from '../app/handler';
+import { getSomeStuffFromAPI } from '../app/handler';
 import lambdaTester from 'lambda-tester';
 
 describe('Auth Code Rabo', () => {
   it('getAuthCode', async () => {
-    return await lambdaTester(GetSomeStuffFromAPI)
+    return await lambdaTester(getSomeStuffFromAPI)
       .event({ something: 'blah' })
       .expectResult((result: any) => {
         expect(result.statusCode).to.equal(200);
